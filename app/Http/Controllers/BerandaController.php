bf60322633b531f302c10985f15ebcf89c82b4d1
syahrulerfian <syahrulerfian0@gmail.com>
2021-02-27 17:05:14 -0800

    inipesan

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -10,9 +10,9 @@ class BerandaController extends Controller
     
     public function index(Request $request)
     {
-       $positif = DB::table('trackings')->sum('trackings.positif');
-       $sembuh = DB::table('trackings')->sum('trackings.sembuh');
-       $meninggal = DB::table('trackings')->sum('trackings.meninggal');
+       $positif = DB::table('trackings')->sum('tracking.positif');
+       $sembuh = DB::table('trackings')->sum('tracking.sembuh');
+       $meninggal = DB::table('trackings')->sum('tracking.meninggal');
        return view("layouts.beranda.index", compact('positif','sembuh', 'meninggal'));
     }
 
